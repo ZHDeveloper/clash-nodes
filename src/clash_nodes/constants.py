6a5d6bc455e992fd4a5cf69a75d2ee315e706bd7
ZhiHua <shenzhihua@zuowenzhitiao.com>
2026-03-28 07:25:35 +0800

    Revert "修改配置；"
    This reverts commit 44c870146278582951ff8df0a750ce926730983b.

--- src/clash_nodes/constants.py
+++ src/clash_nodes/constants.py
@@ -19,5 +19,5 @@ DISCOVERED_SOURCES_PATH = "data/sources.json"
 STATS_PATH = "data/stats.json"
 SUBS_CHECK_CONFIG_PATH = "data/subs-check.yaml"
 
-EXPECTED_OUTPUT_FILES = ["all.yaml", "base64.txt"]
+EXPECTED_OUTPUT_FILES = ["all.yaml", "base64.txt", "mihomo.yaml"]
 DATED_SOURCE_MAX_AGE_DAYS = 7
